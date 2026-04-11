3745d7a92d461a8f8e8a8fb01b2fbf7aa4123785
mahmudpython <mdmhamud3311@gmail.com>
2026-04-11 22:27:57 +0600

    404 Handel

---
+++ src/app/blogs/[blogId]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogDetailPage = () => {
+    return (
+        <div>
+            <h1>My Page Comming Soon</h1>
+        </div>
+    );
+};
+
+export default BlogDetailPage;
