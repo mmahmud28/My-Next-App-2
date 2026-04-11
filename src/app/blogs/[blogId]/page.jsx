0e9a117828bf7046d054368f42b0dfe93fe0e4e6
mahmudpython <mdmhamud3311@gmail.com>
2026-04-11 22:37:04 +0600

    error data handel

--- src/app/blogs/[blogId]/page.jsx
+++ src/app/blogs/[blogId]/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const BlogDetailPage = () => {
+const BlogDetailPage = async ({params}) => {
+    const res = await params;
+    console.log(res);
     return (
         <div>
             <h1>My Page Comming Soon</h1>
