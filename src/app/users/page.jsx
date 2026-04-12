c88e4d921307e8b165c5b5289c2c8fc841620026
mahmudpython <mdmhamud3311@gmail.com>
2026-04-12 22:33:11 +0600

    Add User

---
+++ src/app/users/page.jsx
@@ -0,0 +1,31 @@
+import React, { use } from 'react';
+
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await res.json();
+
+    return (
+        <div>
+            <h2>User Comming Hear</h2>
+            <div className="flex flex-wrap gap-4 mt-4">
+                {
+                    users.map(user => (
+                        <div key={user.id} className="card bg-primary text-primary-content w-96">
+                            <div className="card-body">
+                                <h2 className="card-title text-2xl font-bold">{user.name}</h2>
+                                <p className='text-xl font-semibold'>{user.email}</p>
+                                <p className='text-xl font-semibold'>{user.phone}</p>
+                                <p className='text-xl font-semibold'>{user.website}</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default UsersPage;
