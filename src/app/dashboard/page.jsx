4c93ede1449f15847608266f485f6ef8acc55260
mahmudpython <mdmhamud3311@gmail.com>
2026-04-19 23:07:42 +0600

    Add ALl Commit to details Page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const DashboardPage = () => {
+    const data = ()=> {
+        console.log("Data Details Page");
+        
+    }
     return (
         <div>
             <h2>This is the Dashboard</h2>
